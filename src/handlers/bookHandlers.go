823160dbb87d5eb870ccfd88b7f9ee45765ac998
Fomin Alexey <fomin.alexey@outlook.com>
2024-11-13 14:54:21 +0300

    updateBook

--- src/handlers/bookHandlers.go
+++ src/handlers/bookHandlers.go
@@ -58,5 +58,41 @@ func CreateBook(writer http.ResponseWriter, request *http.Request) {
 
 	writer.WriteHeader(http.StatusCreated) // 401 created
 	json.NewEncoder(writer).Encode(book)
+}
 
+// UpdateBookById function can update book by id.
+func UpdateBookById(writer http.ResponseWriter, request *http.Request) {
+	initHeaders(writer)
+	log.Println("Updating a book...")
+	var userBook models.Book
+
+	decoder := json.NewDecoder(request.Body)
+	decoder.DisallowUnknownFields() //! Отклоняет поля которые неизвестны.
+	err := decoder.Decode(&userBook)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest) // 400 error
+		message := models.Message{Message: "provide json file is invalid."}
+		json.NewEncoder(writer).Encode(message)
+		return
+	}
+
+	id, err := strconv.Atoi(mux.Vars(request)["id"])
+	if err != nil {
+		log.Println("error occurs while parsing id field:", err)
+		writer.WriteHeader(http.StatusBadRequest) // 400 error
+		message := models.Message{Message: "don't use parametr ID us uncasted to int."}
+		json.NewEncoder(writer).Encode(message)
+		return
+	}
+
+	found := models.UpdateBookById(id, userBook)
+	if !found {
+		writer.WriteHeader(http.StatusNotFound) // 404 error
+		message := models.Message{Message: fmt.Sprintf("book with id %v is not exists", id)}
+		json.NewEncoder(writer).Encode(message)
+	} else {
+		updatedBook, _ := models.FindBookByID(id)
+		writer.WriteHeader(http.StatusOK) // 200 error
+		json.NewEncoder(writer).Encode(updatedBook)
+	}
 }
