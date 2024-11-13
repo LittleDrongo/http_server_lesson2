ce19d7b0966be1710c6ef051004160aab02cd5ea
Fomin Alexey <fomin.alexey@outlook.com>
2024-11-13 12:22:43 +0300

    add book and authors models

--- src/models/data.go
+++ src/models/data.go
@@ -1,5 +1,7 @@
 package models
 
+import "slices"
+
 var DB []Book
 
 type Book struct {
@@ -31,9 +33,57 @@ func init() {
 
 }
 
-func FiindBookByID(id int) (Book, error) {
-	var book Book
-	var found bool
+// FindBookByID function find book by id.
+func FindBookByID(id int) (book Book, found bool) {
+	for _, b := range DB {
+		if b.ID == id {
+			book = b
+			found = true
+			break
+		}
+	}
 
-	return Book{}, nil
+	return book, found
+}
+
+func DeleteBookById(id int) (found bool) {
+	for idx, b := range DB {
+		if b.ID == id {
+			DB = slices.Delete(DB, idx, idx+1)
+			found = true
+			return found
+		}
+
+	}
+	return found
+}
+
+func UpdateBookById(id int, book Book) (found bool) {
+	for k, b := range DB {
+		if b.ID == id {
+			if len(b.Title) > 0 {
+				DB[k].Title = b.Title
+			}
+			if len(b.Author.Name) > 0 {
+				DB[k].Author.Name = book.Author.Name
+			}
+
+			if len(b.Author.LastName) > 0 {
+				DB[k].Author.LastName = book.Author.LastName
+			}
+
+			if b.Author.BornYear > 0 {
+				DB[k].Author.BornYear = book.Author.BornYear
+			}
+
+			if b.YearPublished > 0 {
+				DB[k].YearPublished = book.YearPublished
+			}
+			found = true
+		}
+
+	}
+
+	return found
+
 }
